56a446cd393787c463bce8a660cf15bf85ac49a7
AnupongTP <anupong.tepa@gmail.com>
2026-08-21 01:46:59 +0700

    Add config.js

---
+++ assets/js/config.js
@@ -0,0 +1,6 @@
+export const CONFIG = {
+  "APP_NAME": "Village Water Supply Dashboard",
+  "PROVINCE": "พะเยา",
+  "API_URL": "https://script.google.com/macros/s/AKfycbyOAmVNY4dsPMaGcZNrpe1wYacSSENjvvptdi3jGrpHFnVeRKPMH7BUc-Kabrt1GLJhyQ/exec",
+  "USE_MOCK_DATA": false
+};
